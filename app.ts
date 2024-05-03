7f5ef70358fc157da479f3ca7ae6487297e4c657
Fahad warsi <fahad7swarsi@gmail.com>
2024-05-03 21:18:35 +0500

    push wordcounter project to github

---
+++ app.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "Enter you sentence to count: ",
+    type: "string",
+    name: "words",
+  },
+]);
+let wordsCount = answer.words.trim().split(" ");
+console.log(wordsCount);
+console.log(`Your sentence word count is : ${wordsCount.length}`);
